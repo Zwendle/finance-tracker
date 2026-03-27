3f0d879f4b9ae7c1b27544b0235e8009879db938
Zachary Wen <zachary.h.wen@gmail.com>
2026-03-26 20:27:57 -0400

    added navigation component, improved header

--- finance-tracker/components/header-logo.tsx
+++ finance-tracker/components/header-logo.tsx
@@ -4,11 +4,9 @@ import Image from "next/image";
 export const HeaderLogo = () => {
   return (
     <Link href="/">
-      <div className="items-center hidden">
-        <Image src="/logo.svg" alt="Logo" width={28} height={28} />
-        <p className="font-semibold text-white text-2xl ml-2.5">
-          Finance Tracker
-        </p>
+      <div className="items-center hidden lg:flex">
+        <Image src="/logo.svg" alt="Logo" height={28} width={28} />
+        <p className="font-semibold text-white text-2xl ml-2.5">Finance</p>
       </div>
     </Link>
   );
